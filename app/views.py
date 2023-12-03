b84be6b1c9ab19069efeaf5bde34b8758231ff59
rudramahakul19 <rudramahakul2014@gmail.com>
2023-12-03 21:30:39 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def mdb4(request):
+    return render(request,'mdb4.html')
+
+def carousel(request):
+    return render(request,'carousel.html')
